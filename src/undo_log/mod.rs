631aaac6060f19ff68b3c6410ce941e637f308e9
AviAvni <avi.avni@gmail.com>
2023-12-26 15:30:41 +0200

    update

--- src/undo_log/mod.rs
+++ src/undo_log/mod.rs
@@ -1,3 +1,8 @@
-/// cbindgen:ignore
+/*
+ * Copyright FalkorDB Ltd. 2023 - present
+ * Licensed under the Server Side Public License v1 (SSPLv1).
+ */
+
+ /// cbindgen:ignore
 mod undo_log;
 mod undo_log_ffi;
