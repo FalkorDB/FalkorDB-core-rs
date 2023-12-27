62c4e871878d4ad6d5243707c3c295875741acdf
AviAvni <avi.avni@gmail.com>
2023-12-27 10:01:44 +0200

    add cbindgen to build

---
+++ build.rs
@@ -0,0 +1,16 @@
+extern crate cbindgen;
+
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let bin_dir = env::var("FalkorDBRS_BINDIR").unwrap();
+
+    cbindgen::Builder::new()
+      .with_crate(crate_dir)
+      .with_language(cbindgen::Language::C)
+      .with_pragma_once(true)
+      .generate()
+      .expect("Unable to generate bindings")
+      .write_to_file(format!("{bin_dir}/FalkorDBRS.h"));
+}
