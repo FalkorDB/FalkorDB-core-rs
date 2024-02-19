e0569757abe5446ee19466b0765fbd052e5cc18d
Avi Avni <avi.avni@gmail.com>
2024-02-19 10:05:48 +0200

    document alloc function

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,23 @@ unsafe impl GlobalAlloc for FalkorDBAlloc {
         &self,
         layout: Layout,
     ) -> *mut u8 {
+        /*
+         * To make sure the memory allocation by Redis is aligned to the according to the layout,
+         * we need to align the size of the allocation to the layout.
+         *
+         * "Memory is conceptually broken into equal-sized chunks,
+         * where the chunk size is a power of two that is greater than the page size.
+         * Chunks are always aligned to multiples of the chunk size.
+         * This alignment makes it possible to find metadata for user objects very quickly."
+         *
+         * From: https://linux.die.net/man/3/jemalloc
+         */
         let size = (layout.size() + layout.align() - 1) & (!(layout.align() - 1));
-        RedisModule_Alloc.unwrap()(size).cast::<u8>()
+
+        match RedisModule_Alloc {
+            Some(alloc) => alloc(size).cast(),
+            None => panic!("alloc function not found"),
+        }
     }
 
     unsafe fn dealloc(
@@ -30,7 +45,10 @@ unsafe impl GlobalAlloc for FalkorDBAlloc {
         ptr: *mut u8,
         _layout: Layout,
     ) {
-        RedisModule_Free.unwrap()(ptr.cast::<c_void>())
+        match RedisModule_Free {
+            Some(f) => f(ptr.cast()),
+            None => panic!("free function not found"),
+        };
     }
 }
 
