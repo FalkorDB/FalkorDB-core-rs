51b8daadf3aea870b08dc737426fafdb593d7b3d
Avi Avni <avi.avni@gmail.com>
2025-03-25 11:05:44 +0200

    fix z link

--- build.rs
+++ build.rs
@@ -28,13 +28,11 @@ fn main() {
     println!("cargo:rustc-link-search=/rocksdb");
     println!("cargo:rustc-link-lib=static=rocksdb");
     println!("cargo:rustc-link-search=/opt/homebrew/Cellar/snappy/1.2.1/lib");
-    println!("cargo:rustc-link-search=/home/linuxbrew/.linuxbrew/Cellar/snappy/1.2.1/lib");
     println!("cargo:rustc-link-search=/home/avi/repos/snappy/build");
     println!("cargo:rustc-link-search=/snappy/build");
     println!("cargo:rustc-link-lib=static=snappy");
-    // println!("cargo:rustc-link-search=/opt/homebrew/Cellar/lz4/1.10.0/lib");
-    // println!("cargo:rustc-link-search=/home/linuxbrew/.linuxbrew/Cellar/lz4/1.10.0/lib");
-    // println!("cargo:rustc-link-lib=static=lz4");
-    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-search=/zlib");
     println!("cargo:rustc-link-lib=static=z");
+    // println!("cargo:rustc-link-search=/opt/homebrew/Cellar/lz4/1.10.0/lib");
+    // println!("cargo:rustc-link-lib=static=lz4");
 }
